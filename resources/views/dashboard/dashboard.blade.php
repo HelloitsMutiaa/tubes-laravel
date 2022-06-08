c32aca044f1de09e8e221eba43d1be32a1086494
HelloitsMutiaa <89656134+HelloitsMutiaa@users.noreply.github.com>
2022-06-08 23:29:47 +0700

    Update dashboard.blade.php

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -7,7 +7,7 @@
     <h1 style="margin-bottom: 0px;"><span>Forum</span></h1>
     <table class="choice">
         <tr>
-            <td><p>Upload Karyamu</p></td>
+            <td><p><b>Upload Karyamu<b></p></td>
         <td>
             <a href="{{route('dashboard.create')}}"><button class="btn-choice"><i class='bx bxs-file-plus bx-sm'></i></button></a>
         </td>
@@ -36,7 +36,7 @@
         <div class="image-box">   
             <h4><b>{{$post->user->name}}</b></h4>
             {{-- <h2><b>Putrija Malau</b></h2> --}}
-            <img src="/{{$post->image}}" alt="">
+            <a href="{{ route('seepost', $post->id) }}"><img src="/{{$post->image}}" alt=""></a>
             <a href="{{ route('seepost', $post->id) }}" style="text-decoration: none; color: #31485f;"><h4>{{ $post->title }}</h4></a>
         </div>
         @endforeach
